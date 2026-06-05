4b6a456196e3a9fa3c96db0be156ea8c8c5eacf4
lixiaya <lixiaya@users.noreply.github.com>
2026-06-06 00:10:39 +0800

    fix: 默认 LAN IP 改为 10.0.0.1
    - diy.sh: sed 替换 base-files config_generate（运行时 IP）
    - .config: 同步更新 PREINIT_IP/PREINIT_BROADCAST（failsafe IP）

--- diy.sh
+++ diy.sh
@@ -4,3 +4,6 @@
 # dae/daed 已包含在 ImmortalWrt 官方 packages feed 中，无需额外克隆
 # 如需自定义命令在此添加（如安装第三方插件）
 # git clone --depth=1 https://github.com/xxx/xxx.git package/xxx
+
+# 默认 LAN IP 改为 10.0.0.1
+sed -i "s/192\.168\.1\.1/10.0.0.1/g" package/base-files/files/bin/config_generate
